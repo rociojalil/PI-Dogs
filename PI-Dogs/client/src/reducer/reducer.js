260a8a575df197269de5e02d29c712e9a3603234
rociojalil <rojalilparedes@gmail.com>
2021-08-11 18:27:30 -0300

    starting front

---
+++ PI-Dogs/client/src/reducer/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+    breeds: [],
+    filter: [],
+    breedsDetail: {},
+    temps: [],
+}
+
+
+const reducer = (state = initialState, action) => {
+}
+
+
+
+
+
+export default reducer;
