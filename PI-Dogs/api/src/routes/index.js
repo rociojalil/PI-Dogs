2bd85856d9c0941997815d3da093bcd1dec401cd
rociojalil <rojalilparedes@gmail.com>
2021-08-12 19:47:49 -0300

    back definitivo

--- PI-Dogs/api/src/routes/index.js
+++ PI-Dogs/api/src/routes/index.js
@@ -9,120 +9,134 @@ const { v4: uuidv4 } = require("uuid");
 // Ejemplo: const authRouter = require('./auth.js');
 const router = Router();
 
-// lógica dentro del get
-// si no es por query la request: trae toda la info en base de datos + info de api y concateno y mando response
-// si es por query distructuring en url y que me traiga la busqueda desde el otro endpoint de search
-router.get("/dogs", async (req, res, next) => {
-  const { q } = req.query;
-  if (!q) {
-    const infoDb = Dog.findAll();
-    const infoApi = await axios.get(`${API_URL}?apikey=${API_KEY}`);
-    Promise.all([infoDb, infoApi])
-      .then((results) => {
-        const [dataDb, dataApi] = results;
-        const response = dataDb.concat(dataApi.data);
-        res.send(response);
-      })
-      .catch((error) => next(error));
 
-  } else {
-    const infoDb = Dog.findAll({
-      where: { name: q }
-    });
-    const infoApi = await axios.get(`${API_URL2}?q=${q}&apikey=${API_KEY}`);
-
-    Promise.all([infoDb, infoApi])
-      // devuelve promesa le puedo aplicar .then
-      .then((results) => {
-        const [dataDb, dataApi] = results;
-        const response = dataDb.concat(dataApi.data);
-        res.send(response);
-      })
-      .catch((error) => next(error));
-  }
-});
-
-router.get('/dogs/:id', async (req, res, next) => {
-    const {id} = req.params
-    // si la busqueda de perro tiene menos de 4 letras busca en la api
-    if (id.length < 4) {
-        await axios.get(`${API_URL}/${id}?apikey=${API_KEY}`)
-        .then(dogs => res.send(dogs.data))
-        .catch(error => next(error))
-    } else {
-        // si no aca busco en mi base de datos 
-        Dog.findOne({
-            where: {
-                id: id
-            },
-            include: {
-                model: Temperament,
-                attributes: ["name"],
-                through: {
-                    attributes: []
-                }
-            }
-        })
-    }
-})
-
-router.get('/temperament', async (req, res, next) => {try {
-    const dog = await axios.get(`${API_URL}?apikey=${API_KEY}`)
-    const doggy = dog.data.map(t => {
-        return t.temperament
-    })
-    const otr = doggy.map(str => {
-        return str && str.split(', ')
-    })
-
-    const concat = otr.flat()
-
-    const filtrao = concat.filter((str, i) => {
-        return concat.indexOf(str) === i
-        // indexOf la primer letra q encuentre/matchea me lo trae. Devuelve indice del primer valor que encuentra
-        // sort que ordena alfabeticamente y de menor a mayor 
-    }).sort()
-    // => {
-        // if (a > b) return 1
-        // if (a < b) return -1
-        // return 0
-    // })
-    // asi funciona internamente un sort
-    // 
-    const temp = filtrao.map(c => {
-        return {
-            name: c || 'Could not get name'
-        }
-    })
-
-
-    const datos = await Temperament.bulkCreate(temp)
-
-    res.send(datos)
-
-} catch (error) {
-    next(error)
-}
-
-})
-
-// ver tema post
+//post funciona pero en postman no me lo muestra, me muestra objeto vacío {} pero en mi bd si me lo crea
 router.post("/dog", async (req, res, next) => {
-    let { name, height, age, weight, temperament } = req.body;
-    try {
-      await Dog.create({
-        id: uuidv4(),
-        name,
-        height,
-        age,
-        weight,
-      }).then((breed) => res.send(breed.addTemperament(temperament)));
-    } catch (error) {
-      next(error);
-    }
-  });
+	const {name, weight, height, life_span, temperament } = req.body
+	try {
+		const createdDog = await Dog.create({
+			id:uuidv4(),
+			name: name,
+			weight,
+			height,
+			life_span,
+			image: 'https://bit.ly/36J26Nu',
+			db: true
+		}).then((raza) => res.send(raza.addTemperament(temperament)));
+			// const id = uuidv4();
+			// newDog = await Dog.create({id, name, height, weight, life_span})
+			// await newDog.setTemperaments(temperaments)
+		}
+		catch (error) {
+			next(error)
+		}
+	})
+	// primero me traigo todo por req.body
+	// despues creo mi dog en mi bd pero el temperament a parte lo agrego con ADD o SET (metodos del belongTomany)
+	// tambien podria ser un temperament.findAll donde le digo que where: temperament o sea el q me llega x body y ahi le hago el add
+	// con un res.send('perro creado con exito!')
 
+
+// GET TODAS + búsqueda por query
+    router.get("/dogs", async (req, res, next) => {
+	const { q } = req.query;
+
+	if (!q) {
+		
+	  const infoDb = Dog.findAll();
+	  const infoApi = await axios.get(`${API_URL}?apikey=${API_KEY}`);
+	  
+	  Promise.all([infoDb, infoApi])
+		.then((results) => {
+		  const [dataDb, dataApi] = results;
+		  const response = dataDb.concat(dataApi.data);
+		  res.send(response);
+		})
+		.catch((error) => next(error));
   
+	} else {
+	  const infoDb = Dog.findAll({
+		where: { name: q }
+	  });
+	  const infoApi = await axios.get(`${API_URL2}?q=${q}&apikey=${API_KEY}`);
+  
+	  Promise.all([infoDb, infoApi])
+		.then((results) => {
+		  const [dataDb, dataApi] = results;
+		  const response = dataDb.concat(dataApi.data);
+		  res.send(response);
+		})
+		.catch((error) => next(error));
+    }
+	});
+
+// ver detalle buscandolo x id + temperamento
+router.get('/dogs/:id', async (req, res, next) => {
+    const { id } = req.params
+    try {
+        if (id.length > 5) {
+			
+        const dogDb = await Dog.findByPk(
+			id, {
+            include: {
+            model:Temperament 
+            }
+        });
+        
+        return res.json(dogDb);
+        } else {
+            const { data } = await axios.get(`${API_URL}/${id}?apikey=${API_KEY}`)
+            const raza = {
+                id: data.id,
+                name: data.name,
+                image: 'https://cdn2.thedogapi.com/images/' + data.reference_image_id + '.jpg',
+                weight: data.weight.metric,
+                height: data.height.metric,
+                life_span: data.life_span,
+                temperament: data.temperament,
+    };
+        res.send(raza)
+} 
+}
+catch(error){
+	next(error)
+}
+})
+
+//GET TEMPERAMENT
+// vamos a la api y me los traigo con un map + split + flat + sort + map.
+
+router.get('/temperament', async (req, res, next) => {
+	try {
+		const dog = await axios.get(`${API_URL}?apikey=${API_KEY}`)
+
+			const doggy = dog.data.map(i => {	// mapeo para traerme los temperamentos de api
+				return i.temperament
+			})
+			const allJoin = doggy.map(e => { 	// de esos los separo con coma x split
+				return e && e.split(', ')
+			})
+			const order = allJoin.flat().sort(); // flat concatena todos los [] en uno solo + sort para ordenarlos menor a mayor/alfabeticamente
+	
+			const dataArray = new Set(order); 	// almaceno todos estos "nuevos" valores
+			let result = [...dataArray];
+				// const filtrao = concat.filter((str, i) => {
+				// return concat.indexOf(str) === i })
+				// indexOf la primer letra q encuentre/matchea me lo trae. Devuelve indice del primer valor que encuentra
+	
+			const temp = result.map((c) => {
+			return {
+				name: c || 'Could not get name',
+			}
+		})
+			const datos = await Temperament.bulkCreate(temp); // creo en la base de datos todos los temperamentos
+			res.send(datos);
+
+	}  catch (error) {
+			next(error)
+		}
+	});
+
 
 
 module.exports = router;
