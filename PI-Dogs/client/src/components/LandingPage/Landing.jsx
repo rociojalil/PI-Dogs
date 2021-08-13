c81d7ba5eb19f1702d0866318e43d551dd8fdade
rociojalil <rojalilparedes@gmail.com>
2021-08-13 04:42:48 -0300

    landing page

--- PI-Dogs/client/src/components/LandingPage/Landing.jsx
+++ PI-Dogs/client/src/components/LandingPage/Landing.jsx
@@ -11,7 +11,12 @@ function Landing() {
                 <div className={styles.title}>
                         <h1>Welcome to Dogs App!</h1>
 
-                        <p className={styles.texto}> </p>
+                        <p className={styles.texto}>
+                            Looking for a new buddy? <br></br>
+                        Great! Dogs are an excellent company! <br></br>     
+                     Although now you will know all the different breeds that there are and details of them, <br></br> do not forget that stray dogs also have a lot of love to give!
+                        ALL DOGS ARE BEAUTIFUL! <br></br>
+                        Let's get to know them </p>
                         <Link to='/home' style={{ color: "black", textDecoration: "none" }}>
 
                         <div className={styles.main}>
